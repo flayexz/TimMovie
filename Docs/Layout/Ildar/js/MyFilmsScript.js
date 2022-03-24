4ec199c2e71c88c9f4142eba1cad851f4b5b96b4
flayexz <hxjsnsjdkswb@mail.ru>
2022-03-24 22:59:52 +0300

    [update] MyFilmsPage

--- Docs/Layout/Ildar/js/MyFilmsScript.js
+++ Docs/Layout/Ildar/js/MyFilmsScript.js
@@ -1,44 +1,40 @@
 const windowSizeToCutText = 990;
-const windowSizeToSmallFont = 680;
+const windowSizeToMedium = 1400;
+const windowSizeToSmallFont = 570;
 const smallFilmTitleLength = 30;
+
 window.onload = function(){
     applySize();
 }
-
-
 
 window.addEventListener('resize',function(){
     applySize();
 });
 
-
 function applySize(){
     let width = document.documentElement.clientWidth;
     if(width < windowSizeToCutText){
         CutText();
+    }
+    resizeToDefault();
+    if(width < windowSizeToMedium){
         if(width > windowSizeToSmallFont){
+            
             resizeToMedium();
         }
         else{
             resizeToSmall();
         }
     }
-    else{
-        resizeToDefault();
-    }
 }
-
 
 function resizeToMedium(){
-    resizeToDefault();
-    document.querySelectorAll('.film-grade').forEach(x => x.style='margin-left:50px !important');
-    document.querySelectorAll('.film-title').forEach(x => x.style='width:20%');
-    document.querySelector('.navigation-film-name').style = 'width:33%';
+    document.querySelectorAll('.film-title').forEach(x => x.style='width:35%');
+    document.querySelectorAll('.film-duration').forEach(x => x.style='width:20%');
 }
 
-
 function CutText(){
-    this.document.querySelectorAll('.film-title-txt').forEach(d => {
+    document.querySelectorAll('.film-title-txt').forEach(d => {
         if(d.innerText.length > smallFilmTitleLength){
             d.innerText = d.innerText.substr(0,smallFilmTitleLength);
             d.innerText += '...';
@@ -48,17 +44,16 @@ function CutText(){
 
 function resizeToSmall(){
         document.querySelector('.films-container').querySelectorAll('span').forEach(x => x.style='font-size:12px !important');
-        document.querySelector('.navigation').querySelectorAll('span').forEach(x => x.style = 'font-size:10px !important; max-width=40px !important');
-        document.querySelectorAll('.film-title').forEach(x => x.style='width:20%');
-        document.querySelector('.navigation-film-name').style = 'width:33%';
-        document.querySelectorAll('.film-grade').forEach(x => x.style='margin-left:10px !important');
+        document.querySelector('.navigation').querySelectorAll('span').forEach(x => x.style = 'font-size:10px !important;');
+        document.querySelector('.navigation-film-name').style = 'width:300px';
+        document.querySelectorAll('.film-duration').forEach(x => x.style = 'width:150px');   
 }
 
 function resizeToDefault(){
-    document.querySelectorAll('.film-grade').forEach(x => x.style='margin-left:15px !important');
+    document.querySelectorAll('.film-title').forEach(x => x.style='width: 39%;');
     document.querySelector('.films-container').querySelectorAll('span').forEach(x => x.style='font-size:18px !important');
     document.querySelector('.navigation').querySelectorAll('span').forEach(x => x.style = 'font-size:15px !important');
-    document.querySelectorAll('.film-title').forEach(x => x.style='width:40%');
-    document.querySelector('.navigation-film-name').style='width:50%';
-}
+    document.querySelector('.navigation-film-name').style=' width: 48%;';
+    document.querySelectorAll('.film-duration').forEach(x => x.style='width: 15%;');
+}  
 
