ba829396fa86a772b5bdf6a9c1de400f21e10fb2
danilBogo <danil.bogo@gmail.com>
2022-04-06 00:20:54 +0300

    add generic repository

---
+++ TimMovie/TimMovie.Infrastructure/Database/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimMovie.SharedKernel.Interfaces;
+
+namespace TimMovie.Infrastructure.Database;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+{
+    private readonly DbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public Repository(DbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public void Create(TEntity item)
+    {
+        _dbSet.Add(item);
+        Save();
+    }
+
+    public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate) =>
+        _dbSet.AsNoTracking().Where(predicate);
+
+    public void Update(TEntity item)
+    {
+        _context.Entry(item).State = EntityState.Modified;
+        Save();
+    }
+
+    public void Delete(TEntity item)
+    {
+        _dbSet.Remove(item);
+        Save();
+    }
+
+    public void Save() => _context.SaveChanges();
+}
---
+++ TimMovie/TimMovie.SharedKernel/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TimMovie.SharedKernel.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    void Create(TEntity item);
+    IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+    void Update(TEntity item);
+    void Delete(TEntity item);
+    void Save();
+}
