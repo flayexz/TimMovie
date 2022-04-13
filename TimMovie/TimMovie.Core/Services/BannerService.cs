ca3b05e4c2d422cc024f85937e4e97f658c2d408
Marat Elagin <maratelagin@gmail.com>
2022-04-13 19:37:08 +0300

    [add]Add banner service

---
+++ TimMovie/TimMovie.Core/Services/BannerService.cs
@@ -0,0 +1,17 @@
+﻿using TimMovie.Core.Entities;
+using TimMovie.SharedKernel.Interfaces;
+
+namespace TimMovie.Core.Services;
+
+public class BannerService
+{
+    private readonly IRepository<Banner> _bannerRepository;
+
+    public BannerService(IRepository<Banner> bannerRepository)
+    {
+        _bannerRepository = bannerRepository;
+    }
+    
+    public async Task<IEnumerable<Banner>> GetBannersAsync()=> 
+        await _bannerRepository.GetAllAsync();
+}
