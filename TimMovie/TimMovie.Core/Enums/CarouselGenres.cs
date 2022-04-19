3e7eed4aee80dd5b922377d409b4c4e68affe613
Marat Elagin <maratelagin@gmail.com>
2022-04-19 23:32:13 +0300

    [add]Add carouselViewComponent. Fix logic in Carousel_Script.js

---
+++ TimMovie/TimMovie.Core/Enums/CarouselGenres.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace TimMovie.Core.Enums;
+
+public enum CarouselGenres
+{
+    [Description("Для детей")]
+    ForKids,
+    [Description("Семейные")]
+    ForFamily=1,
+    [Description("Комедии")]
+    Comedy = 2,
+    [Description("Драмы")]
+    Dramas = 3,
+    [Description("Триллеры")]
+    Thrillers = 4
+}
---
+++ TimMovie/TimMovie.Web/ViewComponents/CarouselViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TimMovie.Core.Entities;
+using TimMovie.Core.Enums;
+using TimMovie.Core.Services.Films;
+using TimMovie.SharedKernel.Interfaces;
+using TimMovie.Web.ViewModels.FilmCard;
+
+namespace TimMovie.Web.ViewComponents;
+
+public class CarouselViewComponent : ViewComponent
+{
+    private readonly FilmCardService _filmCardService;
+    private readonly IMapper _mapper;
+
+    public CarouselViewComponent(FilmCardService filmCardService, IMapper mapper)
+    {
+        _filmCardService = filmCardService;
+        _mapper = mapper;
+    }
+
+    public IViewComponentResult Invoke(CarouselGenres genreName)
+    {
+        var cards = _mapper.Map<List<FilmCardViewModel>>(_filmCardService.GetFilmCardsByGenre(genreName, 15));
+        return View(cards);
+    }
+}
