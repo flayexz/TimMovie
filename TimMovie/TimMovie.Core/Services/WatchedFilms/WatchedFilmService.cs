25fc326ff6350d375204763caeac4d19a45d493f
flayexz <hxjsnsjdkswb@mail.ru>
2022-05-07 05:10:34 +0300

    [add] watched films page

---
+++ TimMovie/TimMovie.Core/Services/WatchedFilms/WatchedFilmService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using TimMovie.Core.Classes;
+using TimMovie.Core.DTO.WatchedFilms;
+using TimMovie.Core.Entities;
+using TimMovie.Core.Query;
+using TimMovie.Core.Services.Films;
+using TimMovie.Core.Specifications.InheritedSpecifications.FilmSpec.UserFilmWatchedSpec;
+using TimMovie.SharedKernel.Interfaces;
+
+namespace TimMovie.Core.Services.WatchedFilms;
+
+public class WatchedFilmService
+{
+    private readonly IRepository<UserFilmWatched> userFilmWatchedRepository;
+    private readonly IMapper mapper;
+    private readonly FilmService filmService;
+
+    public WatchedFilmService(IRepository<UserFilmWatched> userFilmWatchedRepository, IMapper mapper, FilmService filmService)
+    {
+        this.userFilmWatchedRepository = userFilmWatchedRepository;
+        this.mapper = mapper;
+        this.filmService = filmService;
+    }
+
+    public PaginatedList<WatchedFilmDto> GetPaginatedUserWatchedFilmsByUserId(Guid userId,int pageIndex, int pageSize)
+    {
+        var query = userFilmWatchedRepository.Query
+            .Where(new WatchedFilmByUserIdSpec(userId))
+            .OrderByDescending(watched => watched.Date);
+        
+        var paginatedQuery = query            
+            .Skip((pageIndex - 1) * pageSize)
+            .Take(pageSize);
+
+        var queryExec = new QueryExecutor<UserFilmWatched>(paginatedQuery, userFilmWatchedRepository);
+        var films = queryExec
+            .IncludeInResult(x => x.Film)
+            .GetEntities();
+        
+        var watchedDto = films.Select(x =>
+        {
+            var watchedDto = mapper.Map<WatchedFilmDto>(x);
+            watchedDto.Rating = filmService.GetRating(x.Film) ?? 0;
+            return watchedDto;
+        });
+
+        return new PaginatedList<WatchedFilmDto>(watchedDto, query.Count(), pageIndex, pageSize);
+    }
+}
---
+++ TimMovie/TimMovie.Web/ViewModels/WatchedFilms/WatchedFilmViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TimMovie.Web.ViewModels.WatchedFilms;
+
+public class WatchedFilmViewModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public int Grade { get; set; }
+    public double Rating { get; set; }
+    public string Image { get; set; } = null!;
+    public DateOnly WatchedDate { get; set; }
+}
