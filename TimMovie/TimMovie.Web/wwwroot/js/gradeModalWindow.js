e6f3cbe2e5082b1e9181dbd3dc6dad9ef862be21
danilBogo <danil.bogo@gmail.com>
2022-05-09 03:32:48 +0300

    [add] layout and logic for films grade

---
+++ TimMovie/TimMovie.Web/wwwroot/js/gradeModalWindow.js
@@ -0,0 +1,42 @@
+$(function () {
+    let filmId = document.URL.split('/').pop();
+    let choosedButton = null;
+    $.post({
+        url: "/Film/GetGrade",
+        data: {filmId: filmId},
+        success: function (data) {
+            choosedButton = $(`.gradeNumber:contains(${data})`).first();
+            choosedButton.css("background", "#302a45");
+        }
+    })
+    let gradeNumber = $(".gradeNumber");
+    gradeNumber.click(function (e) {
+        $.post({
+            url: "/Film/SetGrade",
+            data: {filmId: filmId, grade: e.target.innerText},
+            success: function () {
+                gradeNumber.css("background", "#1e1a2e");
+                $(e.target).css("background", "#302a45");
+                $('#modalFilmGrade').modal('hide');
+                choosedButton = $(e.target);
+            }
+        })
+    })
+    gradeNumber.mouseover(function(e) {
+        $(e.target).css({
+            "background": "#B22222",
+            "border-color": "#B22222",
+            "cursor": "pointer"
+        });
+    });
+    gradeNumber.mouseleave(function(e){
+        let background = "#1e1a2e";
+        if (choosedButton[0].innerText === $(e.target)[0].innerText)
+            background = "#302a45";
+        $(e.target).css({
+            "background": background,
+            "border-color": "#1e1a2e",
+            "cursor": "default"
+        });
+    })
+})
