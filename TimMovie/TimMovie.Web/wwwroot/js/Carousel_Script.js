3d19a274e24524f85466c00dd7aad2738be0ae4e
danilBogo <danil.bogo@gmail.com>
2022-05-10 15:21:23 +0300

    [add] grade for main page film card

--- TimMovie/TimMovie.Web/wwwroot/js/Carousel_Script.js
+++ TimMovie/TimMovie.Web/wwwroot/js/Carousel_Script.js
@@ -124,3 +124,11 @@ track.each(number => {
         setMoveCoefficients();
     })
 })
+
+$(".setGradeButton").click(function (e){
+    event.preventDefault()
+    let filmId = $('.film-card__more-info-container').filter(function() {
+        return $(this).css('display') !== 'none';
+    }).attr("href").split('/').pop();
+    getGrade(filmId);
+})
