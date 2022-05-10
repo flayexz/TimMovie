7d3730e55597fad11baead8132f4f50d8a881987
danilBogo <danil.bogo@gmail.com>
2022-05-10 15:20:42 +0300

    [update] refactoring gradeModalWindow.js

--- TimMovie/TimMovie.Web/wwwroot/js/gradeModalWindow.js
+++ TimMovie/TimMovie.Web/wwwroot/js/gradeModalWindow.js
@@ -1,44 +1,46 @@
-$(function () {
-    let filmId = document.URL.split('/').pop();
-    let choosedButton = null;
+let choseButton = null;
+let gradeNumber = $(".gradeNumber");
+let savedFilmId = null;
+function getGrade(filmId){
+    savedFilmId = filmId;
+    gradeNumber.css("background", "#1e1a2e");
     $.post({
         url: "/Film/GetGrade",
         data: {filmId: filmId},
         success: function (data) {
             if (data != null) {
-                choosedButton = $(`.gradeNumber:contains(${data})`).first();
-                choosedButton.css("background", "#302a45");
+                choseButton = $(`.gradeNumber:contains(${data})`).first();
+                choseButton.css("background", "#302a45");
             }
         }
     })
-    let gradeNumber = $(".gradeNumber");
-    gradeNumber.click(function (e) {
-        $.post({
-            url: "/Film/SetGrade",
-            data: {filmId: filmId, grade: e.target.innerText},
-            success: function () {
-                gradeNumber.css("background", "#1e1a2e");
-                $(e.target).css("background", "#302a45");
-                $('#modalFilmGrade').modal('hide');
-                choosedButton = $(e.target);
-            }
-        })
+}
+gradeNumber.click(function (e) {
+    $.post({
+        url: "/Film/SetGrade",
+        data: {filmId: savedFilmId, grade: e.target.innerText},
+        success: function () {
+            gradeNumber.css("background", "#1e1a2e");
+            $(e.target).css("background", "#302a45");
+            $('#modalFilmGrade').modal('hide');
+            choseButton = $(e.target);
+        }
     })
-    gradeNumber.mouseover(function (e) {
-        $(e.target).css({
-            "background": "#B22222",
-            "border-color": "#B22222",
-            "cursor": "pointer"
-        });
+})
+gradeNumber.mouseover(function (e) {
+    $(e.target).css({
+        "background": "#B22222",
+        "border-color": "#B22222",
+        "cursor": "pointer"
     });
-    gradeNumber.mouseleave(function (e) {
-        let background = "#1e1a2e";
-        if (choosedButton != null && choosedButton[0].innerText === $(e.target)[0].innerText)
-            background = "#302a45";
-        $(e.target).css({
-            "background": background,
-            "border-color": "#1e1a2e",
-            "cursor": "default"
-        });
-    })
+});
+gradeNumber.mouseleave(function (e) {
+    let background = "#1e1a2e";
+    if (choseButton != null && choseButton[0].innerText === $(e.target)[0].innerText)
+        background = "#302a45";
+    $(e.target).css({
+        "background": background,
+        "border-color": "#1e1a2e",
+        "cursor": "default"
+    });
 })
