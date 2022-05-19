569e9545b00277939a3b1bb8e55876525165e77c
danilBogo <danil.bogo@gmail.com>
2022-05-19 15:43:09 +0300

    [update] change user status and add migrations

---
+++ TimMovie/TimMovie.Core/Entities/UserStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TimMovie.Core.Entities.Enums;
+using TimMovie.SharedKernel.BaseEntities;
+
+namespace TimMovie.Core.Entities;
+
+public class UserStatus : BaseEntity
+{
+    [ForeignKey("UserGuid")]
+    public User User { get; set; }
+    
+    public UserStatusEnum UserStatusEnum { get; set; }
+    
+    public DateTime DateLastChange { get; set; }
+}
