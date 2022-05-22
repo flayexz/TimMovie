0df5d2c6cb6436f541943f360900632b7ee95f09
flayexz <hxjsnsjdkswb@mail.ru>
2022-05-22 14:45:29 +0300

    [update] startup

--- TimMovie/TimMovie.Web/Program.cs
+++ TimMovie/TimMovie.Web/Program.cs
@@ -11,11 +11,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();
 
+var defaultConnectionString = string.Empty;
+
+if (builder.Environment.EnvironmentName == "Development") {
+    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    // Use connection string provided at runtime by Heroku.
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    var parsedUrl = connectionUrl.Split(";");
+    var host = parsedUrl[0].Split("=")[1];
+    var user = parsedUrl[1].Split("=")[1];
+    var password = parsedUrl[2].Split("=")[1];
+    var database = parsedUrl[3].Split("=")[1];
+
+    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+}
+
+builder.Services.AddDbContext(defaultConnectionString);
+
+builder.Services.ConfigureServices(builder.Configuration);
+
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-builder.Services.ConfigureServices(builder.Configuration);
 
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 {
