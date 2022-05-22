25c4d4dfd537c76d9b647e74898b1ec03cff8313
flayexz <hxjsnsjdkswb@mail.ru>
2022-05-22 03:18:25 +0300

    [sad rabbit]

--- TimMovie/TimMovie.Web/Configuration/ConfigureServices.cs
+++ TimMovie/TimMovie.Web/Configuration/ConfigureServices.cs
@@ -12,7 +12,7 @@ public static class ServicesConfiguration
     public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDistributedMemoryCache();
-        services.AddDbContext(configuration["ConnectionStrings:DefaultConnection"]);
+        services.AddDbContext(configuration.GetConnectionString("DefaultConnection"));
         services.AddIdentity();
         services.AddSignalR(options => { options.ClientTimeoutInterval = new TimeSpan(0, 5, 0); });
 
