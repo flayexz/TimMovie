a67c3eb8ba4266577e836d382eb36725bb08be5b
flayexz <hxjsnsjdkswb@mail.ru>
2022-05-23 16:41:13 +0300

    [fix] upload files style

--- TimMovie/TimMovie.Admin/client/src/components/common/upload/Preview.tsx
+++ TimMovie/TimMovie.Admin/client/src/components/common/upload/Preview.tsx
@@ -15,7 +15,7 @@ export const Preview: FC<Props> = ({setPreview, preview,uploadProps}) => {
     }
 
     return(<>
-        <div className="justify-content-center d-flex flex-column align-items-center">
+        <div className="d-flex flex-column align-items-center">
             <img src={preview} alt="" className={styles.preview} width={uploadProps.photoWidth} height={uploadProps.photoHeight} style={{borderRadius: uploadProps.borderRadius}}/>
             <input type="button" className="btn btn-outline-danger w-100 mt-2" value="Отмена" onClick={removePreview} />
         </div>
