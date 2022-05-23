f4d813abfe4b73a2b6bb094da5c4fd9428a5f576
flayexz <hxjsnsjdkswb@mail.ru>
2022-05-23 23:12:55 +0300

    [add] delete banners

--- TimMovie/TimMovie.Admin/client/src/components/banners/Banner.tsx
+++ TimMovie/TimMovie.Admin/client/src/components/banners/Banner.tsx
@@ -1,19 +1,38 @@
 import {BannerDto} from "../../dto/BannerDto";
 import styles from "./banner.module.css"
+import $api from "../../http";
 
 interface Props {
-    banners: BannerDto[]
+    banners: BannerDto[],
+    setIsSubmitted: Function
 }
 
+
 function Banners(props: Props) {
+
+    function onDelete(bannerId: string) {
+        let formData = new FormData()
+        formData.append('bannerId', bannerId)
+        $api.delete(`/banners/${bannerId}`).then(response => {
+            if (response.data.success) {
+                props.setIsSubmitted(true)
+                alert('баннер удален')
+            } else {
+                alert(response.data.textError)
+            }
+        })
+    }
+
     return (
         <div>
             {
                 props.banners.map((banner) =>
-                    <div className={styles.bannerImage} style={{backgroundImage: `url(${process.env.REACT_APP_FILESERVER + banner.image})`}}>
+                    <div className={styles.bannerImage}
+                         style={{backgroundImage: `url(${process.env.REACT_APP_FILESERVER + banner.image})`}}>
                         <div className={styles.bannerContainer}>
-                                <h1 className={styles.bannerFilmTitle}>{banner.filmTitle}</h1>
-                                <p className={styles.bannerFilmDescription}>{banner.description}</p>
+                            <h1 className={styles.bannerFilmTitle}>{banner.filmTitle}</h1>
+                            <p className={styles.bannerFilmDescription}>{banner.description}</p>
+                            <input type="button" className="btn btn-danger btn-lg" value="Удалить" onClick={() => onDelete(banner.bannerId)}/>
                         </div>
                     </div>
                 )
--- TimMovie/TimMovie.Admin/server/src/modules/bannerModule/bannerController.ts
+++ TimMovie/TimMovie.Admin/server/src/modules/bannerModule/bannerController.ts
@@ -1,7 +1,10 @@
 import {Admin} from "../authModule/adminAuth";
-import {Controller, Get} from "@nestjs/common";
+import {Body, Controller, Delete, Get, Param, Post, Query, UploadedFile, UseInterceptors} from "@nestjs/common";
 import {BannerService} from "./bannerService";
 import {BannerDto} from "../../dto/BannerDto";
+import {FileInterceptor} from "@nestjs/platform-express";
+import {NewBannerDto} from "../../dto/NewBannerDto";
+import {Result} from "../../dto/Result";
 
 
 @Admin()
@@ -13,5 +16,24 @@ export class BannerController {
     @Get('getAllBanners')
     public async GetAllBanners(): Promise<BannerDto[]> {
         return await this.bannerService.GetAllBanners();
+    }
+
+    @Post('add')
+    @UseInterceptors(FileInterceptor('img'))
+    async addNewBanner(@Body() newBanner:NewBannerDto, @UploadedFile() image: Express.Multer.File) : Promise<Result<string>>{
+        if (image == null){
+            return {
+                success: false,
+                textError: "Необходимо добавить обложку для фильма",
+            }
+        }
+
+        return await this.bannerService.addNewBanner(newBanner, image);
+    }
+
+    @Delete('/:bannerId')
+    async deleteBanner(@Param("bannerId") bannerId: string):Promise<Result<string>>{
+        console.log(bannerId)
+        return await this.bannerService.deleteBanner(bannerId)
     }
 }
