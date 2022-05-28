9fc89901e989c35a24f9e9caec70e60baa279af1
danilBogo <danil.bogo@gmail.com>
2022-05-28 04:30:49 +0300

    [update] change comments layout and add dynamic loading

--- TimMovie/TimMovie.Web/wwwroot/js/comments.js
+++ TimMovie/TimMovie.Web/wwwroot/js/comments.js
@@ -1,54 +1,74 @@
-﻿let skip = 0;
-const take = 5;
-const errorColor = "#B22222";
-const successColor = "#157347";
+﻿$(function () {
+    let skip = 0;
+    const take = 5;
+    let allLoaded = false;
+    let isLoading = false;
+    const errorColor = "#B22222";
+    const successColor = "#157347";
+    const url = "/Film/GetCommentsWithPaginationView";
+    window.addEventListener("scroll", tryLoadMoreComments);
+    window.addEventListener("resize", tryLoadMoreComments);
 
-window.onload = function () {
-    skip = 0;
-}
 
-$(".leave-comment-container").on('click', '.button-comment-send', () => {
-    let element = $(".textarea-comment")[0];
-    if (element.value.length < 2) {
-        changeButtonColorAndText("Слишком короткое сообщение", errorColor);
-        return;
+    window.onload = function () {
+        skip = 0;
     }
-    if (element.value.length > 1000) {
-        changeButtonColorAndText("Cлишком длинное сообщение", errorColor);
-        return;
-    }
-    $.post({
-        url: "/Film/LeaveComment",
-        data: {filmId: document.URL.split('/').pop(), content: element.value},
-        success: function (data) {
-            element.value = "";
-            $(".comments-container-body-comments").prepend(data);
-            changeButtonColorAndText("Комментарий добавлен", successColor);
+
+    $(".leave-comment-container").on('click', '.button-comment-send', () => {
+        let element = $(".textarea-comment")[0];
+        if (element.value.length < 2) {
+            changeButtonColorAndText("Слишком короткое сообщение", errorColor);
+            return;
         }
+        if (element.value.length > 1000) {
+            changeButtonColorAndText("Cлишком длинное сообщение", errorColor);
+            return;
+        }
+        $.post({
+            url: "/Film/LeaveComment",
+            data: {filmId: document.URL.split('/').pop(), content: element.value},
+            success: function (data) {
+                element.value = "";
+                $(".comments-container-body-comments").prepend(data);
+                changeButtonColorAndText("Комментарий добавлен", successColor);
+            }
+        });
     });
-});
 
-$(".comments-container-body").on('click', '.button-show-more', () => {
-    skip += 5;
-    $.post({
-        url: "/Film/GetCommentsWithPaginationView",
-        data: {
-            filmId: document.URL.split('/').pop(),
-            take: take,
-            skip: skip
-        },
-        success: function (data) {
-            if (data.trim() === '')
-                $(".button-show-more")[0].style.display = "none";
-            else
+    function tryLoadMoreComments() {
+        if (allLoaded || isLoading) {
+            return;
+        }
+        const height = document.body.offsetHeight;
+        const screenHeight = window.innerHeight;
+        const scrolled = window.scrollY;
+        const threshold = height - screenHeight / 4;
+        const position = scrolled + screenHeight;
+        if (position >= threshold) {
+            isLoading = true;
+            getComments();
+        }
+    }
+
+    function getComments() {
+        skip += take;
+        $.post({
+            url: url,
+            data: {
+                filmId: document.URL.split('/').pop(),
+                take: take,
+                skip: skip
+            },
+            success: function (data) {
+                allLoaded = data.length < take;
                 $(".comments-container-body-comments").append(data);
-        }
-    });
+            }
+        }).then(() => isLoading = false);
+    }
+
+    function changeButtonColorAndText(text, color) {
+        let element = $(".send-comment-status");
+        element.css({"color": `${color}`});
+        element[0].innerText = text;
+    }
 });
-
-function changeButtonColorAndText(text, color){
-    let element = $(".send-comment-status");
-    element.css({"color": `${color}`});
-    element[0].innerText = text;
-}
-
