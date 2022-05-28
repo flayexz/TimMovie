15f40b6a0ecbad852e83b02e13d45b09aadee695
danilBogo <danil.bogo@gmail.com>
2022-05-28 03:13:58 +0300

    [update] change test name

--- TimMovie/TimMovie.WebApi.Tests/Tests.fs
+++ TimMovie/TimMovie.WebApi.Tests/Tests.fs
@@ -8,7 +8,7 @@ type UpdateTests(factory: BaseApplicationFactory<Program>) =
     interface IClassFixture<BaseApplicationFactory<Program>>
     
     [<Fact>]
-    member this.``My test``() =
+     member this.``[TEST] [WITH JWT]Get all user notifications``() =
         let client = factory.CreateClient()
-        let genres = appContext.Genres |> Seq.toList
+//        let genres = appContext.Genres |> Seq.toList
         Assert.True(true)
