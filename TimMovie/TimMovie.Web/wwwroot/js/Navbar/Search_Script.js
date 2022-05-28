22e620f8fc0e9488ce1fdd3cf029bb964ca6ab6e
danilBogo <danil.bogo@gmail.com>
2022-05-29 00:33:19 +0300

    [update] fix Search_Script.js

--- TimMovie/TimMovie.Web/wwwroot/js/Navbar/Search_Script.js
+++ TimMovie/TimMovie.Web/wwwroot/js/Navbar/Search_Script.js
@@ -1,23 +1,52 @@
 $(function () {
-    let element = $('#input-search-entities-navbar')
-    let previousRequest;
-    let currentRequest;
-    element.keyup(function () {
-        let value = element.val();
-        if (value == null || value.trim() === '')
+    let previousRequest = undefined;
+    let currentRequest = null;
+    let element = $('#input-search-entities-navbar');
+
+    $('input').on('keyup', function () {
+        let elm = $(this);
+        let time = (new Date()).getTime();
+        let delay = 150;
+
+        elm.attr({'keyup': time});
+        elm.off('keydown');
+        elm.off('keypress');
+        elm.on('keydown', function (e) {
+            $(this).attr({'keyup': time});
+        });
+        elm.on('keypress', function (e) {
+            $(this).attr({'keyup': time});
+        });
+
+        setTimeout(function () {
+            let oldtime = parseFloat(elm.attr('keyup'));
+            if (oldtime <= (new Date()).getTime() - delay & oldtime > 0 &&
+                elm.attr('keyup') !== '' &&
+                typeof elm.attr('keyup') !== 'undefined') {
+                SendRequest();
+                elm.removeAttr('keyup');
+            }
+        }, delay);
+    });
+
+    function SendRequest() {
+        if (element.val().trim() === '') {
+            $('.search-elements').html("");
             return;
+        }
         previousRequest = currentRequest;
         currentRequest = $.post({
             url: "/Search/SearchEntityResults",
-            data: {namePart: value},
+            data: {namePart: element.val()},
             success: function (data) {
-                $('.search-elements').html(data);
-                previousRequest = null;
+                if ($('#input-search-entities-navbar').val().trim() === '')
+                    $('.search-elements').html("");
+                else
+                    $('.search-elements').html(data);
             }
-        })
-        if (previousRequest !== null)
-            currentRequest.abort();
-    });
+        }).then(() => previousRequest = null)
+    }
+
     $("#buttonClose").click(function () {
         $('#dialogContent').html();
     });
