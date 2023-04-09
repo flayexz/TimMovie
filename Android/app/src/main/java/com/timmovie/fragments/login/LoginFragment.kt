f4361d98effb504269d20bcf0d5a8f24a4765b7a
ash.blade <ashen.blade.dev@gmail.com>
2023-04-09 14:58:14 +0300

    Добавил образцовую страницу логина для примера

--- Android/app/src/main/java/com/timmovie/fragments/login/LoginFragment.kt
+++ Android/app/src/main/java/com/timmovie/fragments/login/LoginFragment.kt
@@ -2,15 +2,23 @@ package com.timmovie.fragments.login
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import com.timmovie.R
+import com.timmovie.fragments.login.components.InputText
 
 class LoginFragment : Fragment() {
 
@@ -26,7 +34,23 @@ class LoginFragment : Fragment() {
     ): View? {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "Fuck gay")
+                var login by remember {
+                    mutableStateOf("")
+                }
+                var password by remember {
+                    mutableStateOf("")
+                }
+                var passwordRepeat by remember {
+                    mutableStateOf("")
+                }
+                LoginPage(
+                    login = login,
+                    loginChange = { login = it },
+                    password = password,
+                    passwordChange = { password = it },
+                    passwordRepeat = passwordRepeat,
+                    passwordRepeatChange = { passwordRepeat = it }
+                )
             }
         }
     }
@@ -36,4 +60,21 @@ class LoginFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
         // TODO: Use the ViewModel
     }
+}
+
+@Composable
+fun LoginPage(login: String, loginChange: (String) -> Unit,
+              password: String, passwordChange: (String) -> Unit,
+              passwordRepeat: String, passwordRepeatChange: (String) -> Unit) {
+    Column {
+        InputText(text = login, onValueChange = loginChange)
+        InputText(text = password, onValueChange = passwordChange)
+        InputText(text = passwordRepeat, onValueChange = passwordRepeatChange)
+    }
+}
+
+@Composable
+@Preview
+fun LoginPagePreview() {
+    LoginPage("Логин", {}, "Пароль", {}, "Пароль", {})
 }
