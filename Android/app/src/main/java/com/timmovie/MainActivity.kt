8f38fa755128ea681a81b76bfcba44649ee8da6e
ash.blade <ashen.blade.dev@gmail.com>
2023-04-19 08:23:25 +0300

    Причесал код

--- Android/app/src/main/java/com/timmovie/MainActivity.kt
+++ Android/app/src/main/java/com/timmovie/MainActivity.kt
@@ -1,22 +1,19 @@
 package com.timmovie
 
+//import com.timmovie.infrastructure.AppModule
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.core.login.NullLoginService
 import com.timmovie.fragments.chat.ChatPage
 import com.timmovie.fragments.chat.ChatViewModel
 import com.timmovie.fragments.login.LoginPage
 import com.timmovie.fragments.login.LoginViewModel
 import com.timmovie.infrastructure.AppModule
-//import com.timmovie.infrastructure.AppModule
 import com.timmovie.infrastructure.AppState
-import com.timmovie.infrastructure.AppStateMachine
 import com.timmovie.theme.TimMovieTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -28,19 +25,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val machine = AppModule.stateMachine
-//            val machine = AppStateMachine()
             val nav = rememberNavController()
             TimMovieTheme {
                 NavHost(navController = nav, startDestination = AppState.Login.name) {
                     composable(AppState.Login.name) {
                         val viewModel = hiltViewModel<LoginViewModel>()
-//                        val viewModel = LoginViewModel(NullLoginService(), machine)
-//                        val viewModel: LoginViewModel by viewModels()
                         LoginPage(viewModel)
                     }
                     composable(AppState.Chat.name) {
                         val viewModel = hiltViewModel<ChatViewModel>()
-//                        val viewModel = ChatViewModel(machine)
                         ChatPage(viewModel)
                     }
                 }
@@ -57,12 +50,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    TimMovieTheme {
-//        LoginPage()
-//    }
-//}
--- Android/app/src/main/java/com/timmovie/fragments/login/LoginPage.kt
+++ Android/app/src/main/java/com/timmovie/fragments/login/LoginPage.kt
@@ -1,8 +1,6 @@
 package com.timmovie.fragments.login
 
-import android.widget.Toast
-import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
